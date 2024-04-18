9f3f0940388c5000347876915fc910f484f008c5
mohamedDVP <mohamed.mak@laposte.net>
2024-04-18 21:39:08 +0200

    formulaire à améliorer

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+    require_once 'header.html';
+?>
+        
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="assets/images/logo_deratech_3d.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Demander un devis</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row">
+                <h2>Nos services</h2>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/deratisation.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Deratisation</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/desinsectisation.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Desinsectisation</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/desinfection.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Desinfection</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+            
+        </div>
+            
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row">
+                <h2>Les nuisibles</h2>
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/rampants.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Les insectes rampants</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/volants.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Les insectes volants</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-md-4 my-2 d-flex">
+                    <div class="card">
+                        <img src="uploads/images/xylophages.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Les insectes xylophages</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's
+                                content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<?php
+    require_once 'footer.html';
+?>
