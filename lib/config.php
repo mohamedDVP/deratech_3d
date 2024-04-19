1a3593dd031b773132d47e8078da9e4682a832cd
mohamedDVP <mohamed.mak@laposte.net>
2024-04-19 19:41:33 +0200

    pull feature/config branch

---
+++ lib/config.php
@@ -0,0 +1,21 @@
+<?php
+
+  $mainMenu = [
+    'index.php' => 'Accueil', 
+    'services.php' => 'Nos services',
+    'insectes.php' => 'Les nuisibles',
+    'contact.php' => 'Nous contacter',
+    'devis.php' => 'Devis'
+  ];
+
+  $dropdownMenuServices = [
+    'deratisation.php' => 'La dératisation',
+    'desinsectisation.php' => 'La désinsectisation',
+    'desinfection.php' => 'La désinfection'
+  ];
+
+  $dropdownMenuNuisibles = [
+    'insectes_rampants.php' => 'Les insectes rampants',
+    'insectes_volants.php' => 'Les insectes volants',
+    'insectes_xylophages.php' => 'Les insectes xylophages'
+  ];
