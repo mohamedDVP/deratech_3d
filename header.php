aafd646a21f1c27210ea32985fe79cf1417d5516
mohamedDVP <mohamed.mak@laposte.net>
2024-04-24 00:41:58 +0200

    fix navbar

--- header.php
+++ header.php
@@ -57,6 +57,34 @@
                                     <a class="nav-link" href="devis.php">Devis</a>
                                 </li>
 
+=======
+                                    foreach($menu as $content) 
+                                    {?>
+                                        <li class="nav-item">
+                                    <?php   if(isset($content['item']) && is_array($content['item']))
+                                            {?>
+                                                <a class="nav-link dropdown-toggle" href="<?= $content['url']; ?>.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <?= $content['name'];?>
+                                                </a>
+                                                <ul class="dropdown-menu">
+                                        <?php   foreach($content['item'] as $subContent)
+                                                {?>
+                                                    <a class="dropdown-item" href="<?= $subContent['url']; ?>.php">
+                                                        <?= $subContent['name']; ?>
+                                                    </a>
+                                                           
+                                        <?php   }?>
+                                                </ul>   
+                                    <?php  }
+                                            else 
+                                            {?>        
+                                                <a class="nav-link active" aria-current="page" href="<?= $content['url'];?>.php">
+                                                    <?= $content['name'];?>
+                                                </a>       
+                                <?php   }?>
+                                        </li> 
+                            <?php   }?>
+>>>>>>> 9d11e8d (fix navbar)
                             </ul>
                         </div>
                     </div>
