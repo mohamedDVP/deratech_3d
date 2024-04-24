31790f9e9c315b3fa2404c7b75d86f566b168ef0
mohamedDVP <mohamed.mak@laposte.net>
2024-04-24 21:02:07 +0200

    add a comment for commit

--- header.php
+++ header.php
@@ -48,8 +48,7 @@
                                                 </a>       
                                 <?php   }?>
                                         </li> 
-                            <?php   }?>
-
+                            <?php /* test */  }?>
                             </ul>
                         </div>
                     </div>
