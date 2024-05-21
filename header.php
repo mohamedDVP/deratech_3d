2dd317fbffabf47211d238cf6ea721e60dda7d18
mohamedDVP <mohamed.mak@laposte.net>
2024-05-21 16:10:18 +0200

    ajout d'un fichier de validation google dans le header

--- header.php
+++ header.php
@@ -12,7 +12,9 @@
         <link rel="stylesheet" href="style.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous">
+        </script>
+            <meta name="google-site-verification" content="tScSMpGa_HDp6iTQBWMGEFbVYtaYB2koOljUx366JZA" />
         <title>Document</title>
     </head>
     <body>
