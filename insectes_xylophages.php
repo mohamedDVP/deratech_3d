5cec973759911f2a1033d5d830c9f805694f0a7f
mohamedDVP <mohamed.mak@laposte.net>
2024-06-25 21:30:49 +0200

    fin insectes xylophages

--- insectes_xylophages.php
+++ insectes_xylophages.php
@@ -4,7 +4,7 @@ require_once ("header.php");
 
 ?>
 
-<h2><div class="container col-xxl-8 px-4 py-5">
+    <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-10 col-sm-8 col-lg-6">
                 <img src="<?= $indexCardsInsectes[2]['image'];?>" class="d-block mx-lg-auto img-fluid" alt="<?= $indexCardsInsectes[2]['titre'];?>" width="700" height="500" loading="lazy">
@@ -14,8 +14,117 @@ require_once ("header.php");
                 <p class="lead"><?= $indexCardsInsectes[2]['contenu'];?></p>
             </div>
         </div>
-    </div></h2>
+    </div>
+    <div class="container col-xxl-8 px-4 py-5">
+        <h3 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Protégez Votre patrimoine</h3>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/dommages-insectes-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Dommages causés par des insectes xylophage" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Qu'est-ce qu'un Insecte Xylophage ?</h4></p>
+                <p>Les insectes xylophages sont des insectes qui se nourrissent de bois. Ils peuvent causer des dommages importants aux structures en bois, meubles, et autres objets en bois dans les maisons et les bâtiments.</p>
+            </div>
+        </div>
 
+        <h2 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Types Courants d'Insectes Xylophages</h2>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/termites-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Termintes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Termites</h4></p>
+                <p>Les termites sont parmi les insectes xylophages les plus destructeurs. Ils vivent en colonies et peuvent dévorer le bois de l'intérieur, souvent sans être détectés pendant longtemps.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/capricorne-des-maison-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Capricorne des maisons" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Capricornes des maisons</h4></p>
+                <p>Les capricornes des maisons attaquent principalement les bois résineux et peuvent causer des dégâts considérables aux structures en bois des bâtiments.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/lyctus-xylophage.jpg" class="d-block mx-lg-auto img-fluid" alt="Lyctus" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Lyctus</h4></p>
+                <p>Les lyctus préfèrent les bois durs et peuvent endommager les parquets, les meubles et les boiseries.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/vrillettes-xylophage.jpg" class="d-block mx-lg-auto img-fluid" alt="Vrillettes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Vrillettes</h4></p>
+                <p>Les vrillettes, aussi connues sous le nom de "vers de bois", peuvent attaquer aussi bien les bois durs que les bois tendres, créant des trous et des galeries dans le bois.</p>
+            </div>
+        </div>
+        <h2 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Nos Solutions pour Éliminer les Insectes Xylophages</h2>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/inspection-expert-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Inspection par un expert" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Inspection et Identification</h4></p>
+                <p>Nos experts réalisent une inspection minutieuse pour identifier les types d'insectes présents et déterminer l'étendue de l'infestation.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/traitement-chimique-xylopgages.jpg" class="d-block mx-lg-auto img-fluid" alt="Traitement chimique" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Traitements Chimiques</h4></p>
+                <p>Nous utilisons des insecticides de qualité professionnelle pour éliminer les insectes xylophages. Ces produits sont appliqués de manière ciblée pour maximiser leur efficacité.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/traitement-injection-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Traitement par injection" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Traitements par Injection</h4></p>
+                <p>Pour les structures en bois fortement infestées, nous utilisons des traitements par injection qui permettent de traiter le bois en profondeur.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/traitement-ecologique-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Traitement écologique" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Traitements Écologiques</h4></p>
+                <p>Pour ceux qui préfèrent des solutions respectueuses de l'environnement, nous offrons des traitements écologiques utilisant des produits naturels et des techniques non toxiques.</p>
+            </div>
+        </div>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/prevention-infestation-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Prévention des infestations" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p class="lead"><h4>Prévention et Suivi</h4></p>
+                <p>Nous fournissons des conseils pratiques pour prévenir les infestations futures et offrons des services de suivi pour assurer un contrôle à long terme.</p>
+            </div>
+        </div>
+        <h2 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Conseils de Prévention</h2>
+        <div class="row flex-lg-row align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="uploads/images/conseils-prevention-insectes-xylophages.jpg" class="d-block mx-lg-auto img-fluid" alt="Conseils de prévention" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <p>Pour éviter les infestations d'insectes xylophages, suivez ces conseils :</p>
+                <ul>
+                    <li><strong>Contrôlez l'Humidité :</strong> Maintenez les niveaux d'humidité bas dans votre maison pour éviter de créer un environnement favorable aux insectes xylophages.</li>
+                    <li><strong>Inspectez Régulièrement :</strong> Examinez régulièrement vos structures en bois pour détecter les signes d'infestation.</li>
+                    <li><strong>Traitez le Bois :</strong> Appliquez des traitements préventifs sur le bois pour le protéger contre les attaques des insectes xylophages.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 <?php
 
